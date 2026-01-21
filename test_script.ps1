e7a1706a81763739edbb49eaa066fd6d561e1add
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2026-01-21 15:06:14 -0800

    Update test_script.ps1

--- test_script.ps1
+++ test_script.ps1
@@ -1,21 +1,6 @@
 
 wsl -l -v
 
-Write-Host "Switching Docker to Linux mode..."
-& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchLinuxEngine
-Start-Sleep -s 20
-docker version
-docker version -f '{{.Server.Os}}'
-
-docker pull busybox --quiet
-docker run --rm -v 'C:\:/user-profile' busybox ls /user-profile
-
-docker pull alpine --quiet
-docker run --rm alpine echo hello_world
-
-Write-Host "Switching Docker to Windows mode..."
-& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine
-Start-Sleep -s 20
 docker version
 docker version -f '{{.Server.Os}}'
 
